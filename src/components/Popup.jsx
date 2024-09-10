29aac968bafcc5de839f1d4a2e825011a764371e
HamidReza <155876163+hamidrjpr2@users.noreply.github.com>
2024-09-10 13:57:53 +0330

    Add files via upload

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import {useState} from 'react'
 
 const Popup = ({ node, setIsPopupOpen, handleAddFolder, handleEditFolder, name, getType, requesttype }) => {
-    const [inputValue, setInputValue] = useState(name)
+    const [inputValue, setInputValue] = useState((input)=> {
+      if (requesttype === 'edit') {
+        return name
+      } else {
+        return ''
+      }
+    })
     const [type, setType] = useState((type) => {
-      if (getType) {
+      if (requesttype === 'edit') {
         return getType
       } else {
         return 'folder'
       }
     })
-    // console.log(type);
+    console.log(requesttype);
   return (
     <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[200px] bg-white justify-center items-center shadow-[1px_1px_10px_1px_rgba(0,0,0,0.5)] rounded-md flex-col flex p-10'>
       <span onClick={() => { setIsPopupOpen(false) }} className='absolute top-1 right-1 text-red-500 cursor-pointer'>X</span>
@@ -20,7 +26,7 @@ const Popup = ({ node, setIsPopupOpen, handleAddFolder, handleEditFolder, name, 
             <option value="folder">Folder</option>
             <option value="file">File</option>
         </select>
-        <button onClick={() => { if (requesttype === 'add') { handleAddFolder(node.id, inputValue, type) } else if (requesttype === 'edit') { handleEditFolder(node.id, inputValue, type) }; setIsPopupOpen(false) }}>{getType === 'add' ? 'Add' : 'Edit'}</button>
+        <button onClick={() => { if (requesttype === 'add') { handleAddFolder(node.id, inputValue, type) } else if (requesttype === 'edit') { handleEditFolder(node.id, inputValue, type) }; setIsPopupOpen(false) }}>Submit</button>
       </div>
     </div>
   )
